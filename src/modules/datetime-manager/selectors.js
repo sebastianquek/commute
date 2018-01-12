6e8f2c7fcc836cbf28f710cba729f9c024b5afca
Sebastian Quek <sebq13@gmail.com>
2018-01-12 22:17:20 +0800

    Fix issue where datetime slider was updating unnecessarily

--- src/modules/datetime-manager/selectors.js
+++ src/modules/datetime-manager/selectors.js
@@ -1,21 +1,22 @@
-// import { createSelector } from 'reselect'
+import { createSelector } from 'reselect'
 import zoneManager from '../zone-manager'
 
-export const brushDomainSelector = state => ({
-  x: state.datetimeBrushDomain.x,
-  y: state.datetimeBrushDomain.y
-})
+export const brushDomainSelector = createSelector(
+  state => state.datetimeBrushDomain.x,
+  state => state.datetimeBrushDomain.y,
+  (x, y) => ({x, y})
+)
 
 export const dateDomainSelector = state => state.datetimeBrushDomain.x
 export const minDateSelector = state => state.datetimeBrushDomain.minX
 export const maxDateSelector = state => state.datetimeBrushDomain.maxX
 export const stepSelector = state => state.datetimeBrushDomain.step
 
-export const ridershipDataSelector = state => {
-  const zoneIds = zoneManager.selectors.allZoneIdsSelector(state)
-  const ridership = zoneIds.reduce((data, key) => {
-    data[key] = state.ridershipData[key] || {}
+export const ridershipDataSelector = createSelector(
+  state => zoneManager.selectors.allZoneIdsSelector(state),
+  state => state.ridershipData,
+  (zoneIds, ridershipData) => zoneIds.reduce((data, key) => {
+    data[key] = ridershipData[key] || {}
     return data
   }, {})
-  return ridership
-}
+)
--- src/modules/zone-manager/selectors.js
+++ src/modules/zone-manager/selectors.js
@@ -1,22 +1,22 @@
+import { createSelector } from 'reselect'
+
 export const originZonesSelector = state => state.zoneManager.categorizedZones.origins
 export const destinationsZonesSelector = state => state.zoneManager.categorizedZones.destinations
-export const allZoneIdsSelector = state => {
-  return [
-    ...state.zoneManager.categorizedZones.origins.map(o => o.id),
-    ...state.zoneManager.categorizedZones.destinations.map(d => d.id)
+
+export const allZoneIdsSelector = createSelector(
+  originZonesSelector,
+  destinationsZonesSelector,
+  (origins, destinations) => [
+    ...origins.map(o => o.id),
+    ...destinations.map(d => d.id)
   ]
-}
-export const zoneColorsSelector = state => {
-  const origins = state.zoneManager.categorizedZones.origins.reduce((origins, o) => {
-    origins[o.id] = o.color
-    return origins
-  }, {})
-  const destinations = state.zoneManager.categorizedZones.destinations.reduce((destinations, d) => {
-    destinations[d.id] = d.color
-    return destinations
-  }, {})
-  return {
-    ...origins,
-    ...destinations
-  }
-}
+)
+
+export const zoneColorsSelector = createSelector(
+  originZonesSelector,
+  destinationsZonesSelector,
+  (origins, destinations) => [
+    ...origins.reduce((obj, o) => ({...obj, [o.id]: o.color}), {}),
+    ...destinations.reduce((obj, d) => ({...obj, [d.id]: d.color}), {})
+  ]
+)
