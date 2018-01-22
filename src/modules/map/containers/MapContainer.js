0cedaedc188998dea2bd6aae033034f5a251feca
Sebastian Quek <sebq13@gmail.com>
2018-01-22 19:16:25 +0800

    Use selectors in map container

--- src/modules/map/containers/MapContainer.js
+++ src/modules/map/containers/MapContainer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Map from '../components/Map'
 import { hoverOverFeature, selectFeature, colorSelectedZones } from '../actions'
+import { mapStyleSelector } from '../selectors'
 import zoneManager from '../../zone-manager'
 // import zoneData from '../../zone-data'
 
@@ -16,9 +17,9 @@ const MapContainer = (props) => {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    mapStyle: state.map.mapStyle,
-    zoneSelectionMode: state.zoneManager.zoneSelectionMode,
-    categorizedZones: state.zoneManager.categorizedZones,
+    mapStyle: mapStyleSelector(state),
+    zoneSelectionMode: zoneManager.selectors.zoneSelectionModeSelector(state),
+    categorizedZones: zoneManager.selectors.allZonesSelector(state),
     ...ownProps
   }
 }
