e346805501d08f8dc08447f33ddc66f7b3058ce5
Sebastian Quek <sebq13@gmail.com>
2018-01-22 21:41:02 +0800

    Get ridership data when step changes

--- src/modules/datetime-manager/sagas.js
+++ src/modules/datetime-manager/sagas.js
@@ -1,30 +1,24 @@
 import moment from 'moment'
 import { select, call, put, take } from 'redux-saga/effects'
 import { requestRidership, requestRidershipError, receiveRidership } from './actions'
-import { REQUEST_RIDERSHIP } from './actionTypes'
+import { REQUEST_RIDERSHIP, SET_STEP } from './actionTypes'
 import { minDateSelector, maxDateSelector, stepSelector } from './selectors'
 import zoneManager from '../zone-manager'
 
 export function * watchAndUpdateRidership () {
   while (true) {
-    const { id } = yield take([REQUEST_RIDERSHIP, zoneManager.actionTypes.ADD_SELECTION])
+    const { id, step } = yield take([REQUEST_RIDERSHIP, SET_STEP, zoneManager.actionTypes.ADD_SELECTION])
 
-    let zoneIds = []
+    const zoneIds = id ? [id] : yield select(zoneManager.selectors.allZoneIdsSelector)
 
     // Get current time window
     const minDate = moment(yield select(minDateSelector))
     const maxDate = moment(yield select(maxDateSelector))
     const duration = moment.duration(maxDate.diff(minDate))
-    const step = yield select(stepSelector)
-
-    if (id) { // Get specific zone's journeys
-      zoneIds.push(id)
-    } else { // Get journeys for all currently selected zones
-      zoneIds = yield select(zoneManager.selectors.allZoneIdsSelector)
-    }
+    const interval = step || (yield select(stepSelector))
 
     try {
-      const journeys = yield call(fetchRidership, zoneIds, minDate, duration, step)
+      const journeys = yield call(fetchRidership, zoneIds, minDate, duration, interval)
       yield put(receiveRidership(journeys))
     } catch (err) {
       yield put(requestRidershipError(err))
