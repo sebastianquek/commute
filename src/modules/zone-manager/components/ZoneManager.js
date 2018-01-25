c7b9f974fd46213efa040ec454fd550ebc6f9104
Sebastian Quek <sebq13@gmail.com>
2018-01-25 22:36:28 +0800

    Zone manager's rollover buttons now overflow over the border. Scrollbar shows on the left side and with of the left side bar changes dynamically

--- src/modules/zone-manager/components/ZoneManager.js
+++ src/modules/zone-manager/components/ZoneManager.js
@@ -3,51 +3,85 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import ZoneCategory from './ZoneCategory'
 
-const AbsoluteWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 2;
-`
-
 const FixedWrapper = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  bottom: ${({theme}) => theme.dimensions.bottomBarHeight};
+  height: 100%;
+  z-index: 2;
+`
+
+const Border = styled.div`
+  position: absolute;
+  top: 0;
+  width: ${({isScrollbarVisible}) => isScrollbarVisible ? '6.5rem' : '5.5rem'};
   border-right: 1px solid ${({theme}) => theme.colors.borderSecondary};
+  background: white;
+  height: 100%;
+  z-index: -1;
+`
+
+const ScrollWrapper = styled.div`
+  height: ${({theme}) => `calc(100vh - ${theme.dimensions.bottomBarHeight})`};
+  direction: rtl;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: flex-end;
   padding: 0.8em 0.6em 0.8em 0.6em;
-  transition: 0.2s all;
-  background: white;
-  overflow-y: auto;
-  overflow-x: hidden;
+  overflow: auto;
 
   * {
+    direction: ltr;
     flex-shrink: 0;
   }
 `
 
-const ZoneManager = ({
-  origins,
-  destinations,
-  setOriginSelectionMode,
-  setDestinationSelectionMode
-}) => {
-  return (
-    <AbsoluteWrapper>
+class ZoneManager extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isScrollbarVisible: false
+    }
+    this.updateScrollbarVisbility = this.updateScrollbarVisbility.bind(this)
+  }
+
+  componentDidMount () {
+    this.updateScrollbarVisbility()
+    window.addEventListener('resize', this.updateScrollbarVisbility)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.updateScrollbarVisbility)
+  }
+
+  componentDidUpdate () {
+    this.updateScrollbarVisbility()
+  }
+
+  updateScrollbarVisbility () {
+    const isScrollbarVisible = this.scrollRef.scrollHeight > this.scrollRef.clientHeight
+    if (isScrollbarVisible !== this.state.isScrollbarVisible) {
+      this.setState({
+        isScrollbarVisible
+      })
+    }
+  }
+
+  render () {
+    return (
       <FixedWrapper>
-        <ZoneCategory onClickAdd={setOriginSelectionMode} category="Origins">
-          {origins}
-        </ZoneCategory>
-        <ZoneCategory onClickAdd={setDestinationSelectionMode} category="Destinations">
-          {destinations}
-        </ZoneCategory>
+        <Border {...this.state}/>
+        <ScrollWrapper innerRef={ref => (this.scrollRef = ref)}>
+          <ZoneCategory onClickAdd={this.props.setOriginSelectionMode} category="Origins">
+            {this.props.origins}
+          </ZoneCategory>
+          <ZoneCategory onClickAdd={this.props.setDestinationSelectionMode} category="Destinations">
+            {this.props.destinations}
+          </ZoneCategory>
+        </ScrollWrapper>
       </FixedWrapper>
-    </AbsoluteWrapper>
-  )
+    )
+  }
 }
 
 ZoneManager.propTypes = {
