2fac7f733beca0b7cf1550eb314b83efd160a69b
Sebastian Quek <sebq13@gmail.com>
2018-02-09 00:08:55 +0800

    Add y axis lines and better align/show duration ticks

--- src/modules/zone-data/components/RouteChoiceChart.js
+++ src/modules/zone-data/components/RouteChoiceChart.js
@@ -6,54 +6,84 @@ import { VictoryAxis, VictoryChart, VictoryZoomContainer, VictoryStack, VictoryB
 
 momentDurationFormatSetup(moment)
 
-const barThickness = 20
-const RouteChoiceChart = (props) => {
-  const keyArray = props.keys.map(({key}) => key)
-  return (
-    <VictoryChart
-      height={(props.keys.length + 1) * barThickness + 90}
-      horizontal={true}
-      domainPadding={{ y: [barThickness, barThickness / 2] }}
-      padding={{ top: 10, right: 20, bottom: 30, left: 120 }}
-      containerComponent={
-        <VictoryZoomContainer
-          zoomDomain={{y: [0, props.keys.length + 1]}}
-          zoomDimension='x'
-          minimumZoom={{x: 180}}
-        />
-      }
-    >
-      <VictoryStack colorScale={'blue'}
-        style={{ labels: { fill: 'white' } }}
-        labelComponent={ <VictoryLabel dx={-60}/> }
+class RouteChoiceChart extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showSeconds: false
+    }
+    this.handleDomainChange = this.handleDomainChange.bind(this)
+  }
+
+  handleDomainChange (domain, props) {
+    const seconds = Math.floor(domain.x[1] - domain.x[0])
+    this.setState({
+      showSeconds: seconds <= 320
+    })
+  }
+
+  render () {
+    const keyArray = this.props.keys.map(({key}) => key)
+    return (
+      <VictoryChart
+        height={(this.props.keys.length + 1) * this.props.barThickness + 110}
+        horizontal={true}
+        domainPadding={{ y: [this.props.barThickness, this.props.barThickness / 2] }}
+        padding={{ top: 10, right: 20, bottom: 50, left: 120 }}
+        containerComponent={
+          <VictoryZoomContainer
+            zoomDomain={{y: [0, this.props.keys.length + 1]}}
+            zoomDimension='x'
+            minimumZoom={{x: 180}}
+            onZoomDomainChange={(domain, props) => this.handleDomainChange(domain, props)}
+          />
+        }
       >
-        {props.data.map((data, index) => {
-          return (
-            <VictoryBar
-              key={index}
-              horizontal={true}
-              categories={{ y: keyArray }}
-              data={props.keys.map(k => ({...k, width: barThickness}))}
-              y={({key}) => data[key] ? data[key].duration : 0}
-              x={({key}) => key}
-              labels={({key}) => data[key] ? data[key].bus : ''}
-            />
-          )
-        })}
-      </VictoryStack>
-      <VictoryAxis
-        tickFormat={t => moment.duration(t, 'seconds').format('h[hrs] m[min]', { minValue: 1 })}
-      />
-      <VictoryAxis
-        dependentAxis
-      />
-    </VictoryChart>
-  )
+        <VictoryStack colorScale={'blue'}
+          style={{ labels: { fill: 'white' } }}
+          labelComponent={ <VictoryLabel dx={-60}/> }
+        >
+          {this.props.data.map((data, index) => {
+            return (
+              <VictoryBar
+                key={index}
+                horizontal={true}
+                categories={{ y: keyArray }}
+                data={this.props.keys.map(k => ({...k, width: this.props.barThickness}))}
+                y={({key}) => data[key] ? data[key].duration : 0}
+                x={({key}) => key}
+                labels={({key}) => data[key] ? data[key].bus : ''}
+              />
+            )
+          })}
+        </VictoryStack>
+        <VictoryAxis
+          tickFormat={t => moment.duration(t, 'seconds')
+            .format(this.state.showSeconds ? 'h[hrs] m[min] s[sec]' : 'h[hrs] m[min]', {
+              trim: 'both'
+            }).replace(' ', '\n')}
+          style={{
+            axis: {stroke: 'black'},
+            grid: {stroke: 'grey', strokeWidth: '1px', opacity: 0.6},
+            ticks: {stroke: 'grey', size: 5}
+          }}
+        />
+        <VictoryAxis
+          dependentAxis
+        />
+      </VictoryChart>
+    )
+  }
 }
 
 RouteChoiceChart.propTypes = {
   data: PropTypes.array,
-  keys: PropTypes.array
+  keys: PropTypes.array,
+  barThickness: PropTypes.number
+}
+
+RouteChoiceChart.defaultProps = {
+  barThickness: 20
 }
 
 export default RouteChoiceChart
