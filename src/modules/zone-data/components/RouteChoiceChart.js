405115b7e92d40232d48d76a1ea16383ab71ef5f
Sebastian Quek <sebq13@gmail.com>
2018-02-09 15:59:45 +0800

    Bus service numbers will only show when there's enough space

--- src/modules/zone-data/components/RouteChoiceChart.js
+++ src/modules/zone-data/components/RouteChoiceChart.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import momentDurationFormatSetup from 'moment-duration-format'
+import { scaleLinear } from 'd3-scale'
 import { VictoryAxis, VictoryChart, VictoryZoomContainer, VictoryStack, VictoryBar, VictoryLabel } from 'victory'
 
 momentDurationFormatSetup(moment)
@@ -9,17 +10,32 @@ momentDurationFormatSetup(moment)
 class RouteChoiceChart extends React.Component {
   constructor (props) {
     super(props)
+    this.startZoomDomainSeconds = 3600
+    this.leftPadding = 120
+    this.rightPadding = 20
+    this.labelYOffset = -16
     this.state = {
+      scale: scaleLinear()
+        .domain([0, this.startZoomDomainSeconds])
+        .range([0, 450 - this.leftPadding - this.rightPadding]),
       showSeconds: false
     }
     this.handleDomainChange = this.handleDomainChange.bind(this)
   }
 
   handleDomainChange (domain, props) {
+    if (domain.x[0] < 0) domain.x[0] = 0
     const seconds = Math.floor(domain.x[1] - domain.x[0])
-    this.setState({
-      showSeconds: seconds <= 320
-    })
+    if (this.state.zoomDomainSeconds !== seconds) {
+      const scale = props.scale.x
+        .domain(domain.x)
+        .range([0, props.width - this.leftPadding - this.rightPadding])
+      this.setState({
+        scale,
+        zoomDomainSeconds: seconds,
+        showSeconds: seconds <= 320
+      })
+    }
   }
 
   render () {
@@ -29,7 +45,7 @@ class RouteChoiceChart extends React.Component {
         height={(this.props.keys.length + 1) * this.props.barThickness + 110}
         horizontal={true}
         domainPadding={{ y: [this.props.barThickness, this.props.barThickness / 2] }}
-        padding={{ top: 10, right: 20, bottom: 50, left: 120 }}
+        padding={{ top: 10, right: this.rightPadding, bottom: 50, left: this.leftPadding }}
         containerComponent={
           <VictoryZoomContainer
             zoomDomain={{y: [0, this.props.keys.length + 1]}}
@@ -41,7 +57,7 @@ class RouteChoiceChart extends React.Component {
       >
         <VictoryStack colorScale={'blue'}
           style={{ labels: { fill: 'white' } }}
-          labelComponent={ <VictoryLabel dx={-60}/> }
+          labelComponent={ <VictoryLabel dx={this.labelYOffset} textAnchor='end' /> }
         >
           {this.props.data.map((data, index) => {
             return (
@@ -52,7 +68,15 @@ class RouteChoiceChart extends React.Component {
                 data={this.props.keys.map(k => ({...k, width: this.props.barThickness}))}
                 y={({key}) => data[key] ? data[key].duration : 0}
                 x={({key}) => key}
-                labels={({key}) => data[key] ? data[key].bus : ''}
+                labels={({key, y}) => {
+                  if (data[key]) {
+                    const bus = data[key].bus
+                    const estimatedWidth = bus.length * 6
+                    const barWidth = this.state.scale(y) - this.state.scale(0) + this.labelYOffset
+                    return barWidth > estimatedWidth ? bus : ''
+                  }
+                  return ''
+                }}
               />
             )
           })}
